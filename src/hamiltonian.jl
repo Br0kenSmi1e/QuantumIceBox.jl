d354686e932b986265976a476a787e25f2adf0f9
LongliZheng <lzheng228@connect.hkust-gz.edu.cn>
2025-11-28 11:32:10 +0800

    update

---
+++ src/hamiltonian.jl
@@ -0,0 +1,7 @@
+function get_system_hamiltonian()
+    
+end
+
+function get_coupling_hamiltonian()
+    
+end
---
+++ src/struct.jl
@@ -0,0 +1,21 @@
+
+"""
+H = \\sum_{m} h_m a_m + \\sum{m, n} u_{m n} a_m a_n 
+"""
+struct QuantumSpinGlass{T}
+    size::Int
+    op::AbstractMatrix{Complex{T}} # a_m
+    field::AbstractVector{T} # h_m
+    coupling::AbstractMatrix{T} # u_{m n}
+end
+
+"""
+H_I = \\sum_{m, n} v_{m n} a_m b_n
+"""
+struct Coupling{T}
+    size1::Int
+    size2::Int
+    op1::AbstractMatrix{Complex{T}} # a_m
+    op2::AbstractMatrix{Complex{T}} # b_n
+    coupling::AbstractMatrix{T} # v_{m n}
+end
