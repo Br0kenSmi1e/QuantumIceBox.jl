38fa01dc03bf42ce841cda02b084d97ea0734cfc
Documenter.jl <documenter@juliadocs.github.io>
2025-11-28 05:16:17 +0000

    build based on 49877e2

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#QuantumIceBox","page":"Home","title":"QuantumIceBox","text":"Documentation for QuantumIceBox.\n\n","category":"section"}]
+[{"location":"#QuantumIceBox","page":"Home","title":"QuantumIceBox","text":"Documentation for QuantumIceBox.\n\n","category":"section"},{"location":"#QuantumIceBox.Coupling","page":"Home","title":"QuantumIceBox.Coupling","text":"HI = \\sum{m, n} v{m n} am b_n\n\n\n\n\n\n","category":"type"},{"location":"#QuantumIceBox.QuantumSpinGlass","page":"Home","title":"QuantumIceBox.QuantumSpinGlass","text":"H = \\sum{m} hm am + \\sum{m, n} u{m n} am an \n\n\n\n\n\n","category":"type"}]
 }
